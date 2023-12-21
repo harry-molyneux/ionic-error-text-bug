0d190f970e132bf58da915ba927c452861332c99
Harry Molyneux <harrymolyneux@hotmail.co.uk>
2023-12-21 12:20:51 +0000

    remove unused pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
-        path: 'list',
-        loadChildren: () => import('./list/list.module').then((m) => m.ListPageModule),
-    },
-    {
-        path: 'grid',
-        loadChildren: () => import('./grid/grid.module').then((m) => m.GridPageModule),
-    },
-    {
         path: 'form',
         loadComponent: () => import('./form/form.page').then((m) => m.FormPage),
     },
